9e588efea765ee8be7624a0c3934e5cc241d9759
rvega <rvega@elsoftwarehamuerto.org>
2010-09-30 16:15:33 -0500

    Initial Import

---
+++ Classes/QREncoderAppDelegate.m
@@ -0,0 +1,23 @@
+//
+//  QREncoderAppDelegate.m
+//  QREncoder
+//
+//  Created by Rafael Vega on 9/29/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "QREncoderAppDelegate.h"
+
+@implementation QREncoderAppDelegate
+
+@synthesize window;
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
+	// Insert code here to initialize your application 
+}
+
+- (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)theApplication{
+	return YES;
+}
+
+@end
